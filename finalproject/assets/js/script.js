22849e7182d5b09d75c6f639c930118c27cb913d
Christine M Jones <cmjones10@ualr.edu>
2026-05-07 01:46:39 +0000

    breaks

--- finalproject/assets/js/script.js
+++ finalproject/assets/js/script.js
@@ -103,3 +103,25 @@ cards.forEach(card => {
   });
 
 });
+/* =========================
+   EXPECTATIONS SLIDESHOWS
+========================= */
+
+const slideshows = document.querySelectorAll(".slideshow");
+
+slideshows.forEach(slideshow => {
+
+  const slides = slideshow.querySelectorAll(".slide");
+  let index = 0;
+
+  setInterval(() => {
+
+    slides[index].classList.remove("active");
+
+    index = (index + 1) % slides.length;
+
+    slides[index].classList.add("active");
+
+  }, 3000);
+
+});
